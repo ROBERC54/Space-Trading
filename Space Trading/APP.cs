2a7d181ee84b07cd551d05112e860168c5246655
Christian Roberts <christian.m.roberts@outlook.com>
2020-02-12 09:24:13 -0500

    Added APP class with Run function

---
+++ Space Trading/APP.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Space_Trading
+{
+    class APP
+    {
+        public void Run()
+        { 
+            
+        }
+    }
+}
