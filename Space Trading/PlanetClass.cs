2e690bf7922239315c0b07e05357d8fe2cddeabe
Mochacycle13 <mmmoore13@gmail.com>
2020-02-13 08:38:39 -0500

    added a planesclass with some extremely minor functionality.

---
+++ Space Trading/PlanetClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace Space_Trading
+{
+    public class PlanetClass
+    {
+        public void Run()
+        {
+            string PlanetPath = "/Users/";
+            string name();
+            //location = Map.GridPnt.random
+            //public moon moon1 =
+            //inventory =
+            //GrossPlanetaryProduct =
+            //motivations =
+            //laws =
+            //inhabitants = list(characters)
+            //sectors =
+            //seasons = 
+        }
+    }
+}
