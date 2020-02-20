d4ed8beefd065f73ec2894e459654d16c3387839
Christian Roberts <christian.m.roberts@outlook.com>
2020-02-20 10:58:23 -0500

    How_To now displays a brief message to the player about the game and exits when any key is pressed back to MainMenu

--- Space Trading/How_To.cs
+++ Space Trading/How_To.cs
@@ -9,8 +9,17 @@ namespace Space_Trading
      //This class will handle the case of "How to" when called from within Main_Menu.cs
      //The class will display a short guide to the player, then return to MainMenu screen
         public void Run()
-        { 
-        
+        {
+            Console.Clear();
+            Console.WriteLine("How To");
+            Console.WriteLine("YOU'RE A SPACE TRADER, BOSS!  TRAVERSE THE FARTHEST REACHES OF SPACE AND TRADE SOME GOODS!");
+            Console.WriteLine("PIONEER YOUR WAY THROUGH THE GALAXY VIA SPACESHIP, BUT WATCH OUT FOR THOSE BLACK HOLES!");
+            Console.WriteLine("BUY! SELL! TRADE! TAKE NO LOSSES ON INVENTORY WHILE STAGING YOUR GETAWAY WITH THE GOODS!");
+            Console.WriteLine("WRANGLE UP THOSE BOMB CUSTOMER SATISFACTION SURVEYS AS YOU DROP THEM OFF EXOTIC MISCELLANEA!");
+            Console.WriteLine("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nPress any key to Continue");
+            Console.ReadKey();
+            Console.Clear();
+            new Main_Menu().Run();
         }
     }
 }
